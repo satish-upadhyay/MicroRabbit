c1a8845f55f3b6b6f5e67e099edf9566f94883ad
Satish-Upadhyay <satish.upadhyay@hotmail.com>
2020-03-29 13:32:54 +0530

    EventBus classes added

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Events/Message.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Message:IRequest<bool>
+    {
+    }
+}
