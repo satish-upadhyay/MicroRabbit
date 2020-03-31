77d2adb02d9e97e5a2944e0f979419b853546b55
Satish-Upadhyay <satish.upadhyay@hotmail.com>
2020-03-31 16:03:33 +0530

    Partially completed Banking Microservices

---
+++ MicroRabbit/MicroRabbit.Banking.Api/Controllers/BankingController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MicroRabbit.Banking.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BankingController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        //private readonly ILogger<BankingController> _logger;
+        private readonly IAccountService _accountService;
+
+        //public BankingController(ILogger<BankingController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        public BankingController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Account>> Get()
+        {
+            return Ok(_accountService.GetAccounts());
+        }
+    }
+}
