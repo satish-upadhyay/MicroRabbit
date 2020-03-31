c9561f1fa201ea6c542e12411641ad064e816d0a
Satish-Upadhyay <satish.upadhyay@hotmail.com>
2020-04-01 00:28:25 +0530

    Added presentation layer for MicroRabbit application

---
+++ BankingDBQueries.sql
@@ -0,0 +1,6 @@
+insert into Accounts(AccountType,AccountBalance)
+values('Checking','0.00')
+insert into Accounts(AccountType,AccountBalance)
+values('Savings','0.00')
+
+select * from Accounts
---
+++ MicroRabbit/MicroRabbit.MVC/Services/TransferService.cs
@@ -0,0 +1,27 @@
+﻿using MicroRabbit.MVC.Models.DTO;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.MVC.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly HttpClient _apiClient;
+
+        public TransferService(HttpClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+        public async Task Transfer(TransferDTO transferDto)
+        {
+            var uri = "http://localhost:5000/api/banking";
+            var transferContent = new StringContent(JsonConvert.SerializeObject(transferDto), System.Text.Encoding.UTF8, "application/json");
+            var response = await _apiClient.PostAsync(uri, transferContent);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
